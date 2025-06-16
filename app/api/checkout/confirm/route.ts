098b2054ece042b8fcb56a21db0a7e81619bc6db
Fernanda  Araiza <fernandaaraiza@Air-de-Fernanda.attlocal.net>
2025-06-16 12:57:51 -0700

    fix: update Supabase import to use createClient

--- app/api/checkout/confirm/route.ts
+++ app/api/checkout/confirm/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
-import { supabase } from '@/utils/supabase/server'
+import { createClient } from '@/utils/supabase/server'
+const supabase = createClient()
+
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2024-06-20',
